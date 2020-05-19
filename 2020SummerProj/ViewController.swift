82b56b6c498d54866e1670120405b83a87d0f025
Jundong Chen <jundong2@illinois.edu.com>
2020-05-19 18:59:52 -0500

    Initial Commit

---
+++ 2020SummerProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020SummerProj
+//
+//  Created by Ted on 5/19/20.
+//  Copyright © 2020 香槟最靓的仔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
