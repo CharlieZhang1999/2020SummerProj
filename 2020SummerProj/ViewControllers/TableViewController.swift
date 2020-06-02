879dfe420e801c3284cc696d5fa179d76e5b8738
gualagugu <jling6@illinois.edu>
2020-06-02 16:46:10 -0500

    adding mapView Collective View in first page

--- 2020SummerProj/ViewControllers/TableViewController.swift
+++ 2020SummerProj/ViewControllers/TableViewController.swift
@@ -1,18 +1,18 @@
 //
-//  firstViewController.swift
+//  TableViewController.swift
 //  2020SummerProj
 //
-//  Created by Ted on 5/21/20.
+//  Created by Emily Ling on 2020/5/31.
 //  Copyright © 2020 香槟最靓的仔. All rights reserved.
 //
 
 import UIKit
 
-class firstViewController: UIViewController {
+class TableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-  
+    
 }
